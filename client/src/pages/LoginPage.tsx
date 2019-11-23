8e4d4d27bcd5625296f9698d03408bab49b9651e
Jack Engelmann <engejack@gmail.com>
2019-11-23 01:24:44 +0100

    style login page

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,21 +1,28 @@
-import React from 'react';
+import React from 'react'
+import './LoginPage.scss'
+import { Button } from '../components/Button'
 
 type Props = {}
 
+const cn = 'login-page'
+
 export function LoginPage(props: Props) {
     return (
-        <form action="login" method="post">
-            <div>
-                <label>Username:</label>
-                <input type="text" name="username" />
-            </div>
-            <div>
-                <label>Password:</label>
-                <input type="password" name="password" />
-            </div>
-            <div>
-                <input type="submit" value="Log In" />
-            </div>
-        </form>
+        <div className={cn}>
+            <form action="login" method="post">
+                <h1>Login</h1>
+                <div className={`${cn}__labeled-input`}>
+                    <label htmlFor="user-name-input">Username</label>
+                    <input id="user-name-input" type="text" name="username" />
+                </div>
+                <div className={`${cn}__labeled-input`}>
+                    <label htmlFor="user-password-input">Password</label>
+                    <input id="user-password-input" type="password" name="password" />
+                </div>
+                <Button className={`${cn}__login-button`}>
+                    Login
+                </Button>
+            </form>
+        </div>
     )
 }
