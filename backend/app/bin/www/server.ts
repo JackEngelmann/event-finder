2ed77466c93138921d8ddbfc61e6c128a29ebe81
Jack Engelmann <engejack@gmail.com>
2024-10-13 18:57:58 +0200

    remove dead code

--- backend/app/bin/www/server.ts
+++ backend/app/bin/www/server.ts
@@ -3,26 +3,8 @@ import app from '../../app'
 
 const PORT = process.env.PORT || 5000
 
-/**
- * Imperatives to start and stop the server
- * (used for testing, hot-reloading)
- */
-
 const server = http.createServer(app).listen({ port: PORT }, () => {
     console.log(`server started on port ${PORT}`)
 })
 
 export default server
-
-/*
-let currentApp = app
-
-if (module.hot) {
-    module.hot.accept(['./server', './schema'], () => {
-        server.removeListener('request', currentApp)
-        server.on('request', app)
-        currentApp = app
-    })
-}
-
-*/
